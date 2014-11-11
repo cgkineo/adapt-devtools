bbe6eb8ea6fd07ba51cb1f8026b3ae3b5fa216cd
Oliver Foster <oliver.foster@kineo.com>
2014-11-11 15:33:03 +0000

    Fix from keyCode to which

--- js/adapt-cheat.js
+++ js/adapt-cheat.js
@@ -9,98 +9,115 @@ define(function(require) {
 	var Adapt = require('coreJS/adapt');
 	var Backbone = require('backbone');
 	
+	var buffer = "";
 
 	Adapt.once('app:dataReady', function() {
 
-		var buffer = "";
-
 		$(window).on("keypress", function(e) {
-			buffer+= String.fromCharCode(e.keyCode).toLowerCase();
-
-			var blen = buffer.length;
-			if (blen > 100) buffer = buffer.substr(1,100);
-			blen = buffer.length
-
-			if (buffer.substr( blen - ("kcheatfail").length, ("kcheatfail").length  ) == "kcheatfail") {
-
-				var currentModel = Adapt.findById(Adapt.location._currentId);
-				switch ( Adapt.location._contentType ) {
-				case "page": case "menu":
-					var componentModels = currentModel.findDescendants("components");
-					var questions = componentModels.where({ _isQuestionType: true});
-					_.each(questions, function(question) {
-						question.set("_score",0);
-						question.set("_attemptsLeft",question.set("_attempts") - 1);
-						question.set("_attempts",1);
-						question.set("_interactions",question.get("_interactions") ? question.get("_interactions") + 1 : 1);
-					});
-					componentModels.each(function(item) {
-						item.set("_isCorrect", false);
-						item.set("_isSubmitted", true);
-						item.set("_isComplete", true);
-						item.set("_isInteractionsComplete", true);
-					})
-					break;
-				}
-
-			} else if (buffer.substr( blen - ("kcheatpass").length, ("kcheatpass").length  ) == "kcheatpass") {
-
-				var currentModel = Adapt.findById(Adapt.location._currentId);
-				switch ( Adapt.location._contentType ) {
-				case "page": case "menu":
-					var componentModels = currentModel.findDescendants("components");
-					var questions = componentModels.where({ _isQuestionType: true});
-					_.each(questions, function(question) {
-						question.set("_score",1);
-						question.set("_attemptsLeft",question.set("_attempts") - 1);
-						question.set("_attempts",1);
-						question.set("_interactions",question.get("_interactions") ? question.get("_interactions") + 1 : 1);
-					});
-					componentModels.each(function(item) {
-						item.set("_isCorrect", true);
-						item.set("_isSubmitted", true);
-						item.set("_isComplete", true);
-						item.set("_isInteractionsComplete", true);
-					})
-					break;
-				}
-
-			} else if (buffer.substr( blen - ("kcheathalf").length, ("kcheathalf").length  ) == "kcheathalf") {
-
-				var currentModel = Adapt.findById(Adapt.location._currentId);
-				switch ( Adapt.location._contentType ) {
-				case "page": case "menu":
-					var componentModels = currentModel.findDescendants("components");
-					var questions = componentModels.where({ _isQuestionType: true});
-					var getright = questions.length / 2;
-					var right = 0;
-					_.each(questions, function(question) {
-						if (right < getright) {
-							right++
-							question.set("_score",1);
-							question.set("_attemptsLeft",question.set("_attempts") - 1);
-							question.set("_attempts",1);
-							question.set("_interactions",question.get("_interactions") ? question.get("_interactions") + 1 : 1);
-						} else {
-							question.set("_score",0);
-							question.set("_attemptsLeft",question.set("_attempts") - 1);
-							question.set("_attempts",1);
-							question.set("_interactions",question.get("_interactions") ? question.get("_interactions") + 1 : 1);
-						}
-					});
-					componentModels.each(function(item) {
-						item.set("_isCorrect", item.get("_score") == 1);
-						item.set("_isSubmitted", true);
-						item.set("_isComplete", true);
-						item.set("_isInteractionsComplete", true);
-					})
-					break;
-				}
-
-			}
+			buffer+= String.fromCharCode(e.which).toLowerCase();
+			processBuffer();
 		})
 
 
 	});
+
+
+	function processBuffer() {
+		var blen = buffer.length;
+		if (blen > 100) buffer = buffer.substr(1,100);
+		blen = buffer.length
+
+		if (buffer.substr( blen - ("kcheatfail").length, ("kcheatfail").length  ) == "kcheatfail") {
+
+			var currentModel = Adapt.findById(Adapt.location._currentId);
+			switch ( Adapt.location._contentType ) {
+			case "page": case "menu":
+				var componentModels = currentModel.findDescendants("components");
+				var questions = componentModels.where({ _isQuestionType: true});
+				_.each(questions, function(question) {
+					question.set("_score",0);
+					question.set("_attemptsLeft",question.set("_attempts") - 1);
+					question.set("_attempts",1);
+					question.set("_interactions",question.get("_interactions") ? question.get("_interactions") + 1 : 1);
+				});
+				componentModels.each(function(item) {
+					item.set("_isCorrect", false);
+					item.set("_isSubmitted", true);
+					item.set("_isComplete", true);
+					item.set("_isInteractionsComplete", true);
+				})
+				break;
+			}
+
+		} else if (buffer.substr( blen - ("kcheatpass").length, ("kcheatpass").length  ) == "kcheatpass") {
+
+			var currentModel = Adapt.findById(Adapt.location._currentId);
+			switch ( Adapt.location._contentType ) {
+			case "page": case "menu":
+				var componentModels = currentModel.findDescendants("components");
+				var questions = componentModels.where({ _isQuestionType: true});
+				_.each(questions, function(question) {
+					question.set("_score",1);
+					question.set("_attemptsLeft",question.set("_attempts") - 1);
+					question.set("_attempts",1);
+					question.set("_interactions",question.get("_interactions") ? question.get("_interactions") + 1 : 1);
+				});
+				componentModels.each(function(item) {
+					item.set("_isCorrect", true);
+					item.set("_isSubmitted", true);
+					item.set("_isComplete", true);
+					item.set("_isInteractionsComplete", true);
+				})
+				break;
+			}
+
+		} else if (buffer.substr( blen - ("kcheathalf").length, ("kcheathalf").length  ) == "kcheathalf") {
+
+			var currentModel = Adapt.findById(Adapt.location._currentId);
+			switch ( Adapt.location._contentType ) {
+			case "page": case "menu":
+				var componentModels = currentModel.findDescendants("components");
+				var questions = componentModels.where({ _isQuestionType: true});
+				var getright = questions.length / 2;
+				var right = 0;
+				_.each(questions, function(question) {
+					if (right < getright) {
+						right++
+						question.set("_score",1);
+						question.set("_attemptsLeft",question.set("_attempts") - 1);
+						question.set("_attempts",1);
+						question.set("_interactions",question.get("_interactions") ? question.get("_interactions") + 1 : 1);
+					} else {
+						question.set("_score",0);
+						question.set("_attemptsLeft",question.set("_attempts") - 1);
+						question.set("_attempts",1);
+						question.set("_interactions",question.get("_interactions") ? question.get("_interactions") + 1 : 1);
+					}
+				});
+				componentModels.each(function(item) {
+					item.set("_isCorrect", item.get("_score") == 1);
+					item.set("_isSubmitted", true);
+					item.set("_isComplete", true);
+					item.set("_isInteractionsComplete", true);
+				})
+				break;
+			}
+
+		}
+	}
+
+	window.kcheatpass = function() {
+		buffer = "kcheatpass";
+		processBuffer();
+	};
+	window.kcheatfail = function() {
+		buffer = "kcheatfail";
+		processBuffer();
+
+	};
+	window.kcheathalf = function() {
+		buffer = "kcheathalf";
+		processBuffer();
+	};
 
 })
