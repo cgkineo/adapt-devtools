adf8dc69c01369eec36fd7b5ac47592a5a53842c
Brad Simpson <brad.simpson@kineo.com>
2024-02-20 08:58:08 -0700

    Use jquery's text()

--- js/map.js
+++ js/map.js
@@ -248,9 +248,11 @@ class CourseMap extends Backbone.Controller {
       if (isStringEmpty(t)) t = this.get('_id');
       
       // Strip HTML tags
-      const title = document.createElement("div");
+      const title = document.createElement('div');
       title.innerHTML = t;
-      return title.textContent || title.innerText || "";
+      t = $(title).text();
+
+      return t;
     }
     function when(prop, options) {
       if (this.get(prop)) {
